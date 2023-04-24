442d8b787651bfe9647ff9df4c721294894fb30d
taku0625 <takutaku06250625@gmail.com>
2023-04-24 13:55:19 +0900

    [WIP] 4/24 at home

--- include/particle_filter/particle_filter.h
+++ include/particle_filter/particle_filter.h
@@ -1,3 +1,6 @@
+#ifndef PARTICLE_FILTER_H
+#define PARTICLE_FILTER_H
+
 #include <ros/ros.h>
 #include <nav_msgs/Odometry.h>
 #include <sensor_msgs/LaserScan.h>
@@ -12,10 +15,15 @@ public:
 protected:
     void initializeParticles(const int particle_num);
 
-    void updateParticlesByOperatingModel(double delta_linear, double delta_angular);
+    void updateParticlesByOperatingModel(const double delta_linear, const double delta_angular);
     void calculateLikelihood();
     void estimatePose();
     void resampleParticles();
+
+    void calculateLikelihoodField();
+    std::vector<int> pMax(const std::vector<double>& laser_ranges);
+    std::vector<double> pRand(const std::vector<double>& laser_ranges);
+    std::vector<double> pHit(const std::vector<double>& laser_ranges);
 
     void odomCallback(const nav_msgs::Odometry::ConstPtr& msg);
     void scanCallback(const sensor_msgs::LaserScan::ConstPtr& msg);
@@ -27,9 +35,25 @@ private:
     ros::Subscriber scan_sub_;
     ros::Subscriber tf_sub_;
 
+    struct Pose
+    {
+        double position_x;
+        double position_y;
+        double angule_z;
+    };
+    Pose pose_ = {0.0, 0.0, 0.0};
+
     std::vector<std::vector<double>> particles_;
-    double odom_[3];
+    std::vector<std::vector<double>> laser_ranges_;
 
     const double ODOM_HZ_ = 50;
-    const double ODOM_NOISES[4] = {4.0, 1.0, 1.0, 4.0};
+    const double ODOM_NOISES_[4] = {4.0, 1.0, 1.0, 4.0};
+    const double SCAN_ANGLE_MIN_ = -2.356194496154785;
+    const double SCAN_ANGLE_MAX_ = 2.356194496154785;
+    const double SCAN_ANGLE_INCREMENT_ = 0.004363323096185923;
+    const double SCAN_RANGE_MAX_ = 30.0;
+    const double MAX_LASER_RANGE_ = 30.0;
+    const double LFM_VAR_ = 0.01;
 };
+
+#endif
--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -2,6 +2,8 @@
 #include <random>
 #include <bits/stdc++.h>
 
+using namespace std;
+
 ParticleFilter::ParticleFilter(ros::NodeHandle& nh, const int particle_nums)
     : nh_(nh)
 {
@@ -23,7 +25,7 @@ void ParticleFilter::initializeParticles(const int particle_nums)
     std::normal_distribution<double> dist_1(0.0, 1.0);
     std::uniform_real_distribution<double> dist_2(-M_PI, M_PI);
 
-    for(int i=0; i < particle_nums; ++i)
+    for(size_t i = 0; i < particle_nums; ++i)
     {
         particles_[i][0] = dist_1(engine);
         particles_[i][1] = dist_1(engine);
@@ -31,18 +33,25 @@ void ParticleFilter::initializeParticles(const int particle_nums)
     }
 }
 
-void ParticleFilter::updateParticlesByOperatingModel(double delta_linear, double delta_angular)
+void ParticleFilter::updateParticlesByOperatingModel(const double delta_linear, const double delta_angular)
 {
+    double linear_noise_std = std::sqrt(
+        ODOM_NOISES_[0] * std::pow(delta_linear, 2.0) + ODOM_NOISES_[1] * std::pow(delta_angular, 2.0)
+    );
+    double angular_noise_std = std::sqrt(
+        ODOM_NOISES_[2] * std::pow(delta_linear, 2.0) + ODOM_NOISES_[3] * std::pow(delta_angular, 2.0)
+    );
+
     std::random_device seed_gen;
     std::default_random_engine engine(seed_gen());
-    std::normal_distribution<double> dist_1(0.0, 1.0);
-    std::normal_distribution<double> dist_1(0.0, 1.0);
+    std::normal_distribution<double> delta_linear_with_noise(delta_linear, linear_noise_std);
+    std::normal_distribution<double> delta_angular_with_noise(delta_angular, angular_noise_std);
 
-    for(int i=0; i < particles_.size(); ++i)
+    for(size_t i = 0; i < particles_.size(); ++i)
     {
-        particles_[i][0] += delta_linear[0] + ;
-        particles_[i][1] = dist_1(engine);
-        particles_[i][2] = dist_2(engine);
+        particles_[i][0] += delta_linear_with_noise(engine) * std::cos(particles_[i][2]);
+        particles_[i][1] += delta_linear_with_noise(engine) * std::sin(particles_[i][2]);
+        particles_[i][2] += delta_angular_with_noise(engine);
     }
 };
 
@@ -58,20 +67,46 @@ void ParticleFilter::resampleParticles()
 {
 };
 
+std::vector<int> ParticleFilter::pMax(const std::vector<double>& laser_ranges)
+{
+    std::vector<int> p(laser_ranges.size());
+    for(size_t i = 0; i < laser_ranges.size(); ++i)
+    {
+        p[i] = laser_ranges[i] == MAX_LASER_RANGE_ ? 1 : 0;
+    }
+    return p;
+}
+
+std::vector<double> ParticleFilter::pRand(const std::vector<double>& laser_ranges)
+{
+    std::vector<double> p(laser_ranges.size());
+    std::fill(p.begin(), p.end(), 1 / MAX_LASER_RANGE_);
+    return p;
+}
+
+std::vector<double> ParticleFilter::pHit(const std::vector<double>& laser_ranges)
+{
+    std::vector<double> p(laser_ranges.size());
+    for(size_t i = 0; i < laser_ranges.size(); ++i)
+    {
+        double laser_angle = pose_.angule_z + SCAN_ANGLE_MIN_ + (double)i * SCAN_ANGLE_INCREMENT_;
+        double laser_position_x = pose_.position_x + laser_ranges[i] * std::cos(laser_angle);
+        double laser_position_y = pose_.position_y + laser_ranges[i] * std::sin(laser_angle);
+    }
+}
+
 void ParticleFilter::odomCallback(const nav_msgs::Odometry::ConstPtr& msg)
 {
     double linear_vel = msg->twist.twist.linear.x;
     double angular_vel = msg->twist.twist.angular.z;
     double delta_linear = linear_vel / ODOM_HZ_;
     double delta_angular = angular_vel / ODOM_HZ_;
-
-    
-    
 }
 
 void ParticleFilter::scanCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
-    ROS_INFO("%f", msg->scan_time);
+    int laser_nums = msg->ranges.size();
+    std::copy(msg->ranges.begin(), msg->ranges.end(), laser_ranges_);
 }
 
 void ParticleFilter::tfCallback(const tf2_msgs::TFMessage::ConstPtr& msg)
